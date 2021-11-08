54250f996da6e74d298b7fa1cfc90da868bd1aa9
Shivam Singh <shivamfaceuit@gmail.com>
2021-11-08 13:50:47 +0530

    Reverse an array using no extra array

--- src/com/company/ReverseArrays.java
+++ src/com/company/ReverseArrays.java
@@ -5,7 +5,19 @@ import java.util.Arrays;
 public class ReverseArrays {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5};
-        reversre(arr);
+//        reversre(arr);
+        rev2(arr);
+    }
+
+    static void rev2(int[] arr) {
+
+        for (int i=0,j=arr.length-1;i<j;i++,j--){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 
     static void reversre(int[] arr) {
